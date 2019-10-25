a06d55b3423093c37223610138c6479e5889336b
horie1024 <ryosuke.pic@gmail.com>
2019-10-25 22:50:59 +0900

    first commit.

---
+++ app/src/main/java/com/horie1024/jetpackcomposebasics/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.horie1024.jetpackcomposebasics
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.Text
+import androidx.ui.core.dp
+import androidx.ui.core.setContent
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Column
+import androidx.ui.layout.Spacing
+import androidx.ui.material.Divider
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.surface.Surface
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                MyScreenContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(child: @Composable() () -> Unit) {
+    MaterialTheme {
+        Surface(color = Color.Yellow) {
+            child()
+        }
+    }
+}
+
+
+
+
+
+@Composable
+fun MyScreenContent() {
+    Column {
+        Greeting("Android")
+        Divider(color = Color.Black)
+        Greeting("There")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", modifier = Spacing(24.dp))
+}
+
+@Preview("Text Preview")
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        MyScreenContent()
+    }
+}
